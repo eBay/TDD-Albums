8a15b0a6fd20c6a7931fc21d30043858afe22490
Van Voorden <rick@northbronson.com>
2015-02-02 22:42:24 -0800

    Initial Commit

---
+++ Albums/Albums.h
@@ -0,0 +1,13 @@
+//
+//  Albums.h
+//  Albums
+//
+//  Created by Van Voorden on 2/2/15.
+//  Copyright (c) 2015 eBay Software Foundation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Albums : NSObject
+
+@end
---
+++ Albums/Albums.m
@@ -0,0 +1,13 @@
+//
+//  Albums.m
+//  Albums
+//
+//  Created by Van Voorden on 2/2/15.
+//  Copyright (c) 2015 eBay Software Foundation. All rights reserved.
+//
+
+#import "Albums.h"
+
+@implementation Albums
+
+@end
