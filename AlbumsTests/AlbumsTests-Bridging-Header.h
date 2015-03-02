8694ad735d4bb1cbab191c4c188b774aa7662d6b
Van Voorden <rick@northbronson.com>
2015-03-01 20:40:54 -0800

    NetworkJSONOperationTestCase

--- AlbumsTests/AlbumsTests-Bridging-Header.h
+++ AlbumsTests/AlbumsTests-Bridging-Header.h
@@ -1,5 +1,6 @@
 #import "TDD_NetworkDataHandler.h"
 #import "TDD_NetworkImageHandler.h"
 #import "TDD_NetworkJSONHandler.h"
+#import "TDD_NetworkJSONOperation.h"
 #import "TDD_NetworkSession.h"
 #import "TDD_NetworkTask.h"
